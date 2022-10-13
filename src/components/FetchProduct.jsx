ffd8bad932163a95dbf67f37ca97b8322a1d4e23
Matheus Wilbert <matheuswilbert2012@gmail.com>
2022-10-13 19:19:05 -0300

    Commit app.js e FetchProduct.jsx

--- src/components/FetchProduct.jsx
+++ src/components/FetchProduct.jsx
@@ -1,39 +1,84 @@
-import React, { useEffect, useState } from 'react';
-
-const FetchProduct = () => {
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Grid,
+    Typography,
+  } from "@mui/material";
+  import React, { useEffect, useState } from "react";
+  
+  const FetchProducts = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [data, setData] = useState([]);
-
+  
     const url = "https://dummyjson.com/products";
-
-
+  
     useEffect(() => {
-        fetch(url)
-            .then(response => response.json())
-            .then(respData => {
-                console.log(respData);
-                setData(respData)
-                setIsLoading(false);
-            })
-            .catch((error) => {
-                console.error(error);
-                setIsLoading(false);
-            })
-    }, [])
-
+      setIsLoading(true);
+      fetch(url)
+        .then((response) => response.json())
+        .then((respData) => {
+          console.log(respData);
+          setData(respData.products);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.error(error);
+          setIsLoading(false);
+        });
+    }, []);
+  
     return (
-        <div>
-            {isLoading ? (
-                <div>Carregando</div>
-            ) : (
-                data.map((produto) => (
-                    <div>
-                        {produto.title}
-                    </div>
-                ))
-            )}
-        </div>
-    )
-}
-
-export default FetchProduct;
+      <Grid container spacing={2} xs={12}>
+        {isLoading ? (
+          <div>Carregando</div>
+        ) : (
+          data.map((produto) => (
+            <Grid
+              item
+              sx={6}
+              xs={12}
+              md={4}
+              lg={1}
+              display="flex"
+              alignItems="stretch"
+            >
+              <Card
+                key={produto.id}
+                style={{
+                  height: "100%",
+                  display: "flex",
+                  alignItems: "stretch",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={produto.thumbnail}
+                  alt={produto.brand}
+                />
+                <CardContent style={{ display: "flex", flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {produto.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {produto.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Compartilhar</Button>
+                  <Button size="small">Conheça mais</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))
+        )}
+      </Grid>
+    );
+  };
+  
+  export default FetchProducts;
