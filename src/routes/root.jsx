93559d8e82eb1e6abb4ad0a8692d0dab83eef1c8
Matheus Wilbert <matheuswilbert2012@gmail.com>
2022-10-20 19:26:10 -0300

    Commit 20-10-22

---
+++ src/routes/root.jsx
@@ -0,0 +1,27 @@
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+    Routes,
+  } from "react-router-dom";
+  import Contador from "../components/Contator";
+  import ErrorPage from "../components/ErrorPage";
+  import FetchNews from "../components/FetchNews";
+  import { PageLayout } from "../components/PageLayout";
+  
+  const route = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<PageLayout />} errorElement={<ErrorPage />}>
+        <Route index path="sobre" element={<Contador />} />
+        <Route path="noticias" element={<FetchNews />} />
+        <Route path="contato" element={<Contador />} />
+      </Route>
+    )
+  );
+  
+  const Root = () => {
+    return <RouterProvider router={route} />;
+  };
+  
+  export default Root;
